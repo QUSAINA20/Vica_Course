030ed6e6589b9a096206b557d389604dc2094d07
QUSAINA20 <91684341+QUSAINA20@users.noreply.github.com>
2023-12-23 19:22:42 +0300

    web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CityController;
+use App\Http\Controllers\Admin\CourseController;
+use App\Http\Controllers\Admin\DurationController;
+use App\Http\Controllers\Admin\RegisterController;
+use App\Http\Controllers\Admin\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +22,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('services', ServiceController::class);
+Route::resource('categories', CategoryController::class);
+Route::resource('cities', CityController::class);
+Route::resource('courses', CourseController::class);
+Route::resource('durations', DurationController::class);
+Route::resource('registers', RegisterController::class)->only('index');
