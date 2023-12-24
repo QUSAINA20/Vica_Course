b4bd5fd4bf3cb9b18a6952f5053288757a815c62
QUSAINA20 <91684341+QUSAINA20@users.noreply.github.com>
2023-12-24 18:27:01 +0300

    api .php and web.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\ServiceController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{category}', [CategoryController::class, 'show']);
+Route::get('/courses/{course}', [CourseController::class, 'show']);
+Route::get('/services', [ServiceController::class, 'index']);
+Route::post('/registers/create', [RegisterController::class, 'store']);
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\CourseController;
 use App\Http\Controllers\Admin\DurationController;
 use App\Http\Controllers\Admin\RegisterController;
 use App\Http\Controllers\Admin\ServiceController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,10 +23,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::middleware(['auth'])->group(function () {
+    Route::resource('services', ServiceController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('cities', CityController::class);
+    Route::resource('courses', CourseController::class);
+    Route::resource('durations', DurationController::class);
+    Route::resource('registers', RegisterController::class)->only('index', 'destroy');
 
-Route::resource('services', ServiceController::class);
-Route::resource('categories', CategoryController::class);
-Route::resource('cities', CityController::class);
-Route::resource('courses', CourseController::class);
-Route::resource('durations', DurationController::class);
-Route::resource('registers', RegisterController::class)->only('index');
+    Route::get('/settings/name-email', [AuthController::class, 'showChangeNameEmailForm'])->name('settings.name.email');
+    Route::put('/settings/name-email', [AuthController::class, 'updateNameEmail'])->name('update.name.email');
+
+    Route::get('/settings/password', [AuthController::class, 'showChangePasswordForm'])->name('settings.password');
+    Route::put('/settings/password', [AuthController::class, 'updatePassword'])->name('update.password');
+
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+});
+
+Route::middleware(['guest'])->group(function () {
+
+    Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+    Route::post('login', [AuthController::class, 'login']);
+});
